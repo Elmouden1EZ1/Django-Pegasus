fc9bc7ccc5ae747b5638dd74f2217c9f0f282c48
Elmouden1EZ1 <hafidaelmouden68@gmail.com>
2025-08-10 09:37:44 +0100

    Premier commit

---
+++ assets/site-bootstrap.js
@@ -0,0 +1,11 @@
+import './styles/site-bootstrap.scss';
+
+// load all of bootstrap's javascript.
+// import 'bootstrap'; // If you wanted to import all of Bootstrap JS
+
+// you can also explicitly include individual bootstrap plugins here like this.
+// this is the minimum needed for the app's default functionality
+import 'bootstrap/js/dist/collapse';  // mobile menus
+import 'bootstrap/js/dist/dropdown';  // navigation dropdowns
+import 'bootstrap/js/dist/alert';  // alerts (close button)
+import 'bootstrap/js/dist/modal';  // modals (used by teams)
